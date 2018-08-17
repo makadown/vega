870495831afe5af91a7b6d45fda38e26d14bff3a
Mario Serrano <maseflores79@gmail.com>
2018-08-17 12:22:20 -0700

    Comenzando a meter filtros

--- src/app/components/vehicle-list/vehicle-list.component.ts
+++ src/app/components/vehicle-list/vehicle-list.component.ts
@@ -11,6 +11,7 @@ import { VehicleService } from '../../services/vehicle.service';
 export class VehicleListComponent implements OnInit {
 
   vehicles: Vehicle[];
+  allVehicles: Vehicle[];
   makes: KeyValuePair[];
   filter: any = {};
 
@@ -19,11 +20,28 @@ export class VehicleListComponent implements OnInit {
   ngOnInit() {
     this.vehicleService.getMakes().subscribe( (makes: any) => this.makes = makes);
     this.vehicleService.getVehicles()
-      .subscribe( (vehicles: any) => this.vehicles = vehicles);
+      .subscribe( (vehicles: any) => this.vehicles = this.allVehicles = vehicles);
   }
 
   onFilterChange() {
+   // console.log('Entrando a onFilterChange()');
+    let vehicles = this.allVehicles;
+  //  console.log('Filtrando makeId ' + this.filter.makeId);
+    if (this.filter.makeId) {
+      vehicles = vehicles.filter( v => +v.make.id === +this.filter.makeId );
+    }
+//    console.log('Filtrando modelId ' + this.filter.modelId);
+    if (this.filter.modelId) {
+      vehicles = vehicles.filter( v => +v.model.id === +this.filter.modelId );
+    }
 
+    this.vehicles = vehicles;
+
+  }
+
+  resetFilter() {
+      this.filter = {};
+      this.onFilterChange();
   }
 
 }
